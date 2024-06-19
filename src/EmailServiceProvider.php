722fec851841e9027c4bfde66ef3ae332b0fb78d
Stanley Aloh <saloh@parkway.ng>
2024-06-19 22:05:11 +0100

    adding updates

--- src/EmailServiceProvider.php
+++ src/EmailServiceProvider.php
@@ -14,8 +14,8 @@ class EmailServiceProvider extends ServiceProvider
         ], 'email-assets');
 
         $this->publishes([__DIR__ . '/../login/index.html' => resource_path('views/email-template/login.blade.php')], 'readycash.template');
-        $this->publishes([__DIR__ . '/../credit mail/index.html' => resource_path('views/email-template/credit-mail.blade.php')], 'readycash.template');
-        $this->publishes([__DIR__ . '/../welcome mail/index.html' => resource_path('views/email-template/welcome-mail.blade.php')], 'readycash.template');
+        $this->publishes([__DIR__ . '/../credit mail/index.html' => resource_path('views/email-template/credit.blade.php')], 'readycash.template');
+        $this->publishes([__DIR__ . '/../welcome mail/index.html' => resource_path('views/email-template/welcome.blade.php')], 'readycash.template');
     }
 
     public function register()
