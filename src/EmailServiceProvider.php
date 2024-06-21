5c2b55d0f62f3a5cc09c2ccf98266ff9a5efce19
Stanley Aloh <saloh@parkway.ng>
2024-06-21 13:29:33 +0100

    fix naming issues

--- src/EmailServiceProvider.php
+++ src/EmailServiceProvider.php
@@ -14,7 +14,7 @@ class EmailServiceProvider extends ServiceProvider
         ], 'email-assets');
 
         $this->publishes([__DIR__ . '/../login/index.html' => resource_path('views/email-template/login.blade.php')], 'readycash.template');
-        $this->publishes([__DIR__ . '/../credit mail/index.html' => resource_path('views/email-template/credit.blade.php')], 'readycash.template');
+        $this->publishes([__DIR__ . '/../credit mail/index.html' => resource_path('views/email-template/dedit.blade.php')], 'readycash.template');
         $this->publishes([__DIR__ . '/../welcome mail/index.html' => resource_path('views/email-template/welcome.blade.php')], 'readycash.template');
     }
 
